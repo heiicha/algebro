4112a160856f86dd581ef975c67817ea2636a326
Ayaan Jain <ayaanjainsg@gmail.com>
2025-08-01 10:37:56 +0800

    Added AI Chatbot for user feedback

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,9 @@
 from google import genai
 from google.genai import types
 
-client = genai.Client()
+
+client = genai.Client(api_key=)
+
 
 def get_feedback(question, answer):
 
@@ -9,14 +11,21 @@ def get_feedback(question, answer):
     question: {question}, with this answer: {answer}. Please give them feedback.
     Additionally in a new line put Answer: 1, if you think they need to try a
     similar question, or Answer: 0, if you think they need to try a different question.
+    Output absolutely nothing else on the last line.
     """
     content = f""
 
     response = client.models.generate_content(
-        model = "gemini-2.5-flash", contents=content,
+        model = "gemini-2.5-flash", contents=prompt,
         config=types.GenerateContentConfig(
             thinking_config=types.ThinkingConfig(thinking_budget=0)
         ),
     )
-    return response.text
-
+    output = response.text
+    result = output.split("\n")
+    answer = False
+    if result[-1][-1] == 0:
+        answer = True
+    result.pop(-1)
+    feedback = "\n".join(result)
+    return [output, answer]
